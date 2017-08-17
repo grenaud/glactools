6a848e7ab5be992a013568b823fc486d99be4ee8
Gabriel <gabriel.reno@gmail.com>
2017-08-17 15:18:27 +0200

    First pages commit

--- AlleleInfo.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * AlleleInfo
- * Date: Aug-29-2012 
- * Author : Gabriel Renaud gabriel.reno [at sign here ] gmail.com
- *
- */
-
-#include "AlleleInfo.h"
--- AlleleInfoReader.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * AlleleInfoReader
- * Date: Aug-29-2012 
- * Author : Gabriel Renaud gabriel.reno [at sign here ] gmail.com
- *
- */
-
-#include "AlleleInfoReader.h"
