8e25a726d0c1ea5ef5dda93d8c0636930b167a3b
Gabriel Renaud <gabriel@wonton>
2017-09-05 12:00:46 +0200

    added EIGEN

---
+++ EIGENSTRAT2ACF.cpp
@@ -0,0 +1,563 @@
+
+#include "EIGENSTRAT2ACF.h"
+
+
+// #include "BAMTableObj.h"
+// #include "BAMTABLEreader.h"
+
+static unsigned int limitToReOpenFP = 200; //if the coordinate is this far away, we will re-open the file pointer
+
+using namespace std;
+
+
+
+void EIGENSTRAT2ACF::setVarsEPO(ReadTabix * rtEPO,string & epoChr,unsigned int & epoCoord,bool & cpgEPO,char & allel_ref,char & allel_chimp,char & allel_anc,bool & lineLeftEPO,string & lineFromEPO){
+
+    lineLeftEPO=(rtEPO->readLine( lineFromEPO ));
+    if(!lineLeftEPO){
+	cerr<<"Error, missing data in the EPO file"<<endl;
+	exit(1);
+    }
+
+    vector<string> fieldsEPO  = allTokens(lineFromEPO,'\t');
+    epoChr                   = fieldsEPO[0];
+    epoCoord                 = string2uint(fieldsEPO[1]);					
+    if(fieldsEPO[9] == "1")
+	cpgEPO=true;		    
+    else
+	cpgEPO=false;		    
+
+
+    allel_ref   = fieldsEPO[2][0];//reference allele
+    allel_anc   = fieldsEPO[3][0];//inferred ancestor
+    allel_chimp = fieldsEPO[4][0];//chimp;
+
+
+}
+
+
+
+EIGENSTRAT2ACF::EIGENSTRAT2ACF(){
+
+}
+
+EIGENSTRAT2ACF::~EIGENSTRAT2ACF(){
+
+}
+
+
+string EIGENSTRAT2ACF::usage() const{
+    const string usage=string("")+ " eigen2acf [options] <EIGENSTRAT prefix> "+"\n"+
+			      "\nThis program convert EIGENSTRAT files into ACF (prints to the stdout)\n"+
+			      
+			      "\t"+"--fai [file]" + "\t\t"+"Fasta index for genome (produced by \"samtools faidx\") (default: none)\n"+
+			      "\t"+"-u" + "\t\t\t"+"Produce uncopressed output (default: "+booleanAsString(uncompressed)+")\n"+
+
+			      "\t"+"--epo [EPO file]"       +"\t" +"Use file as EPO alignment to set the (default: none)\n"+   
+			      "\t"+"                "       +"\t" +"ancestral/root alleles for hominin samples\n"+   
+		  
+			      "";
+    return usage;
+}
+
+int EIGENSTRAT2ACF::run(int argc, char *argv[]){
+
+    // cout<<INT_MAX<<endl;
+    // bool ancAllele           = false;
+
+    int lastOpt=1;
+			      
+
+    //last arg is program name
+    for(int i=1;i<(argc);i++){ 
+	if((string(argv[i]) == "-")  ){
+            lastOpt=i;
+            break;	    
+	}
+
+        if(string(argv[i])[0] != '-' ){
+            lastOpt=i;
+            break;
+        }                               
+
+	if(strcmp(argv[i],"--epo") == 0 ){
+            epoFile=string(argv[i+1]);
+	    epoFileB=true;
+            i++;
+            continue;
+	}
+
+        if(string(argv[i]) == "-u"){
+	    uncompressed=true;
+            continue;
+        }
+
+        if(string(argv[i]) == "--fai"){
+            fastaIndex=string(argv[i+1]);
+            i++;
+            continue;
+        }
+
+	// if(strcmp(argv[i],"--useanc") == 0 ){
+	//     ancAllele       =true;
+	//     continue;
+	// }
+
+
+	cerr<<"Wrong option "<<argv[i]<<endl;
+	exit(1);
+    }
+
+
+    if(lastOpt != (argc-1)){
+	cerr<<"The last argument is <EIGENSTRAT prefix> "<<endl;
+	return 1;		
+    }
+
+
+    if(fastaIndex.size()==0){
+	cerr<<"Must specify fai file "<<endl;
+	return 1;	
+    }
+
+    if(!epoFileB){
+    	cerr<<"Must specify EPO file as this file format does not have the reference information "<<endl;
+    	return 1;	
+    }
+
+    vector<chrinfo> chrFound;
+    uint64_t genomeLength;
+    readFastaIndex(fastaIndex,chrFound,genomeLength);
+
+    //cerr<<"Filter used: "<<*filtersVCF<<endl;
+    //VCFreader vcfr   (string(argv[argc-3]),5);
+    // BAMTABLEreader btr  (),5);
+    string inputFileEIGprefix = string(argv[lastOpt]);
+
+    string genoFile   = inputFileEIGprefix+".geno";
+    string snpFile    = inputFileEIGprefix+".snp";
+    string indFile    = inputFileEIGprefix+".ind";
+
+   if(!isFile(genoFile)){
+        cerr<<".geno file "<<genoFile<<" does not exist"<<endl;
+        exit(1);
+    }
+
+   if(!isFile(snpFile)){
+        cerr<<".snp file "<<snpFile<<" does not exist"<<endl;
+        exit(1);
+   }
+   
+   if(!isFile(indFile)){
+        cerr<<".ind file "<<indFile<<" does not exist"<<endl;
+        exit(1);
+    }
+
+   igzstream myfileG;
+   myfileG.open(genoFile.c_str(), ios::in);
+
+    if (!myfileG.good()){
+        cerr << "Unable to open file "<<genoFile<<endl;
+        return 1;
+    }
+
+    igzstream myfileS;
+    myfileS.open(snpFile.c_str(), ios::in);
+
+    if (!myfileS.good()){
+        cerr << "Unable to open file "<<snpFile<<endl;
+        return 1;
+    }
+
+    igzstream myfileI;
+    myfileI.open(indFile.c_str(), ios::in);
+
+    if (!myfileI.good()){
+        cerr << "Unable to open file "<<indFile<<endl;
+        return 1;
+    }
+
+
+
+    
+    
+
+    string epoFileidx = epoFile+".tbi";
+
+    string epoChr;
+    unsigned int epoCoord;
+
+    ReadTabix * rtEPO =NULL;
+    string lineFromEPO;
+    bool lineLeftEPO;
+    bool cpgEPO=false;
+    bool firstLine=true;
+    char allel_chimp;
+    char allel_ref;
+    char allel_anc;
+
+    // 75060,
+    // 75070,
+    // 5);
+    //header
+    unsigned int totalRec=0;
+    unsigned int writtenRec=0;
+
+    string header="";
+
+    header+="#ACF\n";
+    string programLine;
+    for(int i=0;i<(argc);i++){ 
+	programLine+=(string(argv[i])+" ");
+    }
+
+    header+="#PG:"+programLine+"\n";;
+    header+="#GITVERSION: "+returnGitHubVersion(argv[-1],"")+"\n";;
+
+    header+="#DATE: "+getDateString()+"\n";;
+    header+="#EIGEN2ACF:\n";
+    map<string,uint16_t> chr2index;
+    uint16_t     chrCurrentIndex=0;
+
+    for(unsigned j=0;j<(chrFound.size());j++){
+        header+= string("#SQ\tSN:")+chrFound[j].name+"\tLN:"+stringify(chrFound[j].length)+"\n";
+        chr2index[chrFound[j].name]=chrCurrentIndex++;
+        if(chrCurrentIndex == 0xFFFF){
+            cerr<<"Too many chromosomes for this build, more than 65535"<<endl;
+            return 1;
+        }
+    }
+
+
+
+    header+="#chr\tcoord\tREF,ALT\troot\tanc";
+    bool foundPop=false;
+    string linei;
+    int sizePopRead=0;
+    while ( getline (myfileI,linei)){   
+	//header<<line<<endl;
+	vector<string> fields = allTokens( linei ,' ');
+	if(fields.size()!=3){
+	    cerr<<"Line "<<linei<<" should have 3 TAB deliminated fields"<<endl;
+	    return 1;
+	}
+	sizePopRead++;
+	header+="\t"+fields[0];
+	foundPop=true;
+    }
+    myfileI.close();
+    
+    if(!foundPop){
+	cerr<<"Did not find any data in the individual file"<<endl;
+	return 1;
+    }
+
+    header+="\n";
+    // cout<<header<<endl;
+    // return 1;
+
+    GlacWriter * gw=NULL;
+
+    gw = new GlacWriter(sizePopRead,    //gp.getSizePops(),
+                        false, //gp.isGLFormat(),
+                        2,//gp.isACFormat()?2:1,
+			1,//compression threads
+                        uncompressed);
+    
+
+    if(!gw->writeHeader(header)){
+	cerr<<"GlacViewer: error writing header "<<endl;
+	exit(1);
+    }
+
+
+
+    
+
+
+    string lineG;//geno
+    string lineS;//snp
+    
+    bool hasDataG= (bool)getline (myfileG,lineG);
+    bool hasDataS= (bool)getline (myfileS,lineS);
+
+    while (hasDataG ){
+	if(!hasDataS){	    
+	    cerr<<"eigen2acf: data seems to be missing in the .snp file, stopped at line in .geno "<<lineG<<endl;
+	    return 1;
+	}
+
+
+	
+	totalRec++;
+	// cout<<lineS<<endl;
+	// cout<<lineG<<endl;
+
+	vector<string> token= allTokens(lineS,'\t');
+	if(token.size() != 6){
+	    cerr<<"Error, SNP line should contain 6 fields"<<endl;
+	    return 1;
+	}
+	
+	//token[0];//SNPname
+	string chr          = token[1];//chr
+	//token[2];//genetic position
+	unsigned int pos    = destringify<unsigned int>(token[3]);//physical position
+	char genotypeRef    = token[4][0];
+	char genotypeAlt    = token[5][0];
+
+	
+
+	if(firstLine){
+	    firstLine=false;
+	    if(epoFileB){
+
+		rtEPO = new ReadTabix( epoFile.c_str()  , 
+				       epoFileidx.c_str()  , 
+				       chr,
+				       int(pos),INT_MAX ); //the destructor should be called automatically
+
+		setVarsEPO(rtEPO,epoChr,epoCoord,cpgEPO,allel_ref,allel_chimp,allel_anc,lineLeftEPO,lineFromEPO);
+	    }
+	}
+
+	if(epoFileB){
+
+	    if(!lineLeftEPO){
+		cerr<<"Error, no data in the EPO file "<< chr <<":"<< int(pos) <<endl;
+		return 1;
+	    }
+
+	    if(epoChr != chr){
+		if( (pos - epoCoord ) >= limitToReOpenFP){ //seeking in the file
+		    rtEPO->repositionIterator(chr , int(pos),INT_MAX);
+		}	       	    
+		setVarsEPO(rtEPO,epoChr,epoCoord,cpgEPO,allel_ref,allel_chimp,allel_anc,lineLeftEPO,lineFromEPO);
+	    }
+
+
+
+	    while(epoCoord != pos){
+		if(epoCoord > pos){
+		    cerr<<"Error, are all the sites in EPO there? Difference between coords "<<(lineS)<<"\t"<<lineFromEPO<<endl;
+		    return 1;
+		}
+		
+		if( (pos - epoCoord ) >= limitToReOpenFP){ //seeking in the file
+		    rtEPO->repositionIterator(chr , int(pos),INT_MAX);
+		}
+		
+	    
+		setVarsEPO(rtEPO,epoChr,epoCoord,cpgEPO,allel_ref,allel_chimp,allel_anc,lineLeftEPO,lineFromEPO);
+	    }
+
+
+	    if(epoCoord != pos){
+		cerr<<"Error, are all the sites in EPO there? Difference between coords "<<lineS<<"\t"<<lineFromEPO<<endl;
+		return 1;
+	    }
+	}//end if epoFileB
+
+	char alt='N';
+	string s="ACGT";
+	string chimpString;
+	string ancString;
+	SingleAllele root;
+	SingleAllele anc;
+	AlleleRecords arToWrite (false);
+	bool refIsAltfromSNPfile=false;	
+	//need an allele that is A,C,G,T
+	if(!isResolvedDNA(genotypeRef)){
+	    goto nextline;
+	}
+	if(!isResolvedDNA(genotypeAlt)){
+	    goto nextline;
+	}
+
+	if(epoFileB){
+	    if(isResolvedDNA(allel_ref)){
+		if(allel_ref != genotypeRef){
+		    //cerr<<"eigen2acf: WARNING: The reference allele between the EPO ("<<allel_ref<<") and the ref from the .geno file ("<<genotypeRef<<") disagrees at chr:pos "<<chr<<":"<<pos<<endl;
+		    if(allel_ref != genotypeAlt){
+			cerr<<"eigen2acf: WARNING: The reference allele between the EPO ("<<allel_ref<<") and the alt from the .geno file ("<<genotypeRef<<") also disagrees at chr:pos "<<chr<<":"<<pos<<", skiping"<<endl;			
+			goto nextline;
+		    }else{
+			refIsAltfromSNPfile=true;
+			char c_     = genotypeRef;
+			genotypeRef = genotypeAlt;
+			genotypeAlt = c_;
+		    }
+		    
+		}else{
+		    //fine
+		}
+	    }else{
+		goto nextline;
+	    }
+	    
+	}
+	
+	//determine alternative allele
+	// for(int i=0;i<4;i++){
+	//     if(s[i] != allel_ref){
+	// 	if(genotype.find(s[i]) != string::npos){ //new non-ref allele found
+	// 	    //toprint->hasAllele(i+1) ){
+	// 	    alt=s[i];
+	// 	}
+	//     }
+	// }
+
+
+	if(!epoFileB){ //no epo file
+
+	    root.setRefCount(0); root.setAltCount(0);  root.setIsCpg(false); 
+	    anc.setRefCount(0);  anc.setAltCount(0);   anc.setIsCpg(false); 
+
+	}else{
+	    //unresolved ancestral allele (A,C,G,T)
+	    if(!isResolvedDNA(allel_chimp)){
+		//chimpString="0,0:0"; 
+		root.setRefCount(0); root.setAltCount(0);  root.setIsCpg(false); 
+	    }
+	    //resolved ancestral allele
+	    else{
+		if(allel_chimp == allel_ref){//no diff between chimp and reference
+		    //chimpString="1,0:"+string(cpgEPO?"1":"0");
+		    root.setRefCount(1); root.setAltCount(0);  root.setIsCpg(cpgEPO); 
+
+		}else{
+		    if(alt == 'N'){//no alt defined, the chimp becomes the alt			    
+			alt = allel_chimp;
+			//chimpString="0,1:"+string(cpgEPO?"1":"0");
+			root.setRefCount(0); root.setAltCount(1);  root.setIsCpg(cpgEPO); 							
+		    }else{
+			if(alt == allel_chimp){//alt is chimp
+			    //chimpString="0,1:"+string(cpgEPO?"1":"0");
+			    root.setRefCount(0); root.setAltCount(1);  root.setIsCpg(cpgEPO); 
+			}else{ //tri-allelic site, discard
+			    //continue;
+			    goto nextline;
+			}
+		    }
+		}
+	    }
+
+
+	    if(!isResolvedDNA(allel_anc)){
+		//ancString="0,0:0"; 
+		root.setRefCount(0); root.setAltCount(1);  root.setIsCpg(cpgEPO); 
+	    }
+	    //resolved ancestral allele
+	    else{
+		if(allel_anc == allel_ref){//no diff between ancestor and reference
+		    //ancString="1,0:"+string(cpgEPO?"1":"0");
+		    anc.setRefCount(1); anc.setAltCount(0);  anc.setIsCpg(cpgEPO); 
+
+		}else{
+		    if(alt == 'N'){//no alt defined, the ancestor becomes the alt			    
+			alt = allel_anc;
+			//ancString="0,1:"+string(cpgEPO?"1":"0");			    
+			anc.setRefCount(0); anc.setAltCount(1);  anc.setIsCpg(cpgEPO); 
+
+		    }else{
+			if(alt == allel_anc){//alt is ancestor
+			    //ancString="0,1:"+string(cpgEPO?"1":"0");
+			    anc.setRefCount(0); anc.setAltCount(1);  anc.setIsCpg(cpgEPO); 
+			}else{ //tri-allelic site, discard
+			    //continue;
+			    goto nextline;
+			}
+		    }
+		}
+	    }
+	}
+
+	
+	if(alt!='N'){
+	    if(genotypeAlt != alt ){   // has alternative
+		cerr<<"eigen2acf: WARNING The alternative allele between the EPO and the .geno file disagrees, could be triallelic at chr:pos "<<chr<<":"<<pos<<", skipping"<<endl;		
+		goto nextline;
+	    }	    
+	}
+
+
+
+
+
+	arToWrite.chri          = chr2index[chr];
+	arToWrite.coordinate    = pos;
+	arToWrite.sizePops      = uint32_t(sizePopRead);
+	arToWrite.ref           = genotypeRef;
+	arToWrite.alt           = genotypeAlt;
+		
+	arToWrite.vectorAlleles = new vector<SingleAllele>  ();
+	// SingleAllele root;
+	// SingleAllele anc;
+	
+	arToWrite.vectorAlleles->push_back(root);
+	arToWrite.vectorAlleles->push_back(anc);
+
+	for(unsigned int i=0;i<lineG.size();i++){
+	    char g = lineG[i];
+	    int refCount=0;
+	    int altCount=0;
+
+	    
+	    //homo ref
+	    if(g == '2'){		
+		if(!refIsAltfromSNPfile){
+		    refCount = 2;  altCount = 0;
+		}else{
+		    refCount = 0;  altCount = 2;
+		}
+	    }
+
+	    //het
+	    if(g == '1'){
+		refCount = 1;  altCount = 1;//refIsAltfromSNPfile does not matter
+	    }
+	    
+	    //homo alt
+	    if(g == '0'){
+		if(!refIsAltfromSNPfile){
+		    refCount = 0;  altCount = 2;
+		}else{
+		    refCount = 2;  altCount = 0;
+		}
+	    }
+
+	    if(g == '9'){
+		refCount = 0;  altCount = 0;
+	    }
+
+	    
+	    SingleAllele sample (refCount, altCount, 0);//no CpG provided
+
+	    arToWrite.vectorAlleles->push_back(sample);
+	}//for each char in lineG
+	
+	if(!gw->writeAlleleRecord(&arToWrite)){
+	    cerr<<"eigen2acf: error writing header "<<endl;
+	    exit(1);
+	}	    
+	
+	writtenRec++;
+	
+    nextline:
+        
+        hasDataG = (bool)getline (myfileG,lineG);
+        hasDataS = (bool)getline (myfileS,lineS);
+	
+    }//while hasData
+
+
+    if(rtEPO != NULL)
+	delete(rtEPO);
+    
+    delete(gw);
+
+    cerr<<"Program "<<argv[0]<<" looked at  "<<totalRec<<" records, wrote "<<writtenRec<<" terminated gracefully"<<endl;
+
+    return 0;
+}
+
---
+++ EIGENSTRAT2ACF.h
@@ -0,0 +1,43 @@
+/*
+ * EIGENSTRAT2ACF
+ * Date: Jul-30-2017 
+ * Author : Gabriel Renaud gabriel.reno [at sign here] gmail.com
+ *
+ */
+
+#ifndef EIGENSTRAT2ACF_h
+#define EIGENSTRAT2ACF_h
+
+#include <string>
+#include <climits>
+#include <gzstream.h>
+
+#include "utils.h"
+#include "GlacWriter.h"
+#include "GlacParser.h"
+#include "AlleleRecords.h"
+#include "AlleleInfo.h"
+#include "ReadTabix.h"
+#include "GlactoolsOperations.h"
+
+using namespace std;
+
+class EIGENSTRAT2ACF{
+private:
+    bool uncompressed=0;    
+    string     epoFile      = "none";
+    bool       epoFileB     = false;
+    string     fastaIndex   = "";
+    void setVarsEPO(ReadTabix * rtEPO,string & epoChr,unsigned int & epoCoord,bool & cpgEPO,char & allel_ref,char & allel_chimp,char & allel_anc,bool & lineLeftEPO,string & lineFromEPO);
+
+public:
+    EIGENSTRAT2ACF();
+    EIGENSTRAT2ACF(const EIGENSTRAT2ACF & other);
+    ~EIGENSTRAT2ACF();
+    EIGENSTRAT2ACF & operator= (const EIGENSTRAT2ACF & other);
+    
+    string usage() const;
+    int run(int argc, char *argv[]);
+
+};
+#endif
